9746614c9a08c6014679ed2db2fef03310b079ae
David Tran <davtran303@gmail.com>
2021-03-24 18:05:00 -0600

    new props

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import EmployeeTracker from "./pages/EmployeeTracker";
 import Search from "./pages/Search";
+import Card from ".//components/Card"
 import Wrapper from "./components/Wrapper";
 
 function App() {
@@ -9,7 +10,19 @@ function App() {
     <Router>
       <div>
         <Wrapper>
+          
           <Route exact path="/" component={EmployeeTracker} />
+          {this.state.friends.map(friend => (
+          <Card
+        first = {users.name.first}
+        last = {users.name.last}
+         gender = {users.gender}
+         email = {users.email}
+         age = {users.dob.age}
+         picture = {users.picture.thumbnail}
+          id = {id}
+          />
+        ))}
           <Route exact path="/search" component={Search} />
         </Wrapper>
       </div>
--- practiceapp/src/components/Card/index.js
+++ practiceapp/src/components/Card/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./style.css";
-import axios from "axios";
+
 
 function Card(props) {
   
   return (
-    <div>
-      <h1>Bro This is the card</h1>
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Gender:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.location}
+          </li>
+          <li>
+            <strong>Age:</strong> {props.location}
+          </li>
+          <li>
+            <strong>Picture:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+
     </div>
   );
 }
